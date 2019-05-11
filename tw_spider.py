9415315a6a5b49bb30ff7d8c46d76afb6cacedb3
zhanbao2000 <zhanbao2000@gmail.com>
2019-05-11 17:18:38 +0800

    Add files via upload

--- tw_spider.py
+++ tw_spider.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python
 # coding:utf-8
 
+# 内容 时间
+
 import requests
-import json
 import re
+from pyquery import PyQuery
 
 
-keyword = '%23%E3%83%90%E3%83%B3%E3%83%89%E3%83%AA%E5%8D%94%E5%8A%9B%20%23%E3%82%AC%E3%83%AB%E3%83%91%E5%8D%94%E5%8A%9B'
+# d = PyQuery(filename="H:\\Developer\\Python\\Telegram bots\\Bandori_KRK_bot\\output.html")
+# keyword = '%23%E3%83%90%E3%83%B3%E3%83%89%E3%83%AA%E5%8D%94%E5%8A%9B%20%23%E3%82%AC%E3%83%AB%E3%83%91%E5%8D%94%E5%8A%9B'
+def gettw(keyword, number=5):
+    result = []
+    r = requests.get('https://twitter.com/i/search/timeline?f=tweets&vertical=default&q=' + keyword,
+                     headers={'User-Agent': 'Mozilla'})
+    r.encoding = "utf-8"
+    d = PyQuery(r.json()['items_html'])
+    for i in range(1, number):
+        char = d('.js-tweet-text-container').eq(i-1).text()
+        re.sub('@\n', "@", char, count=0, flags=0)
+        re.sub("#\n", "#", char, count=0, flags=0)
+        result.append(char)
+    return result
 
-timeline_Url = 'https://twitter.com/search?q=' + keyword
-r = requests.get('https://twitter.com/i/search/timeline?f=tweets&vertical=default&q=' + keyword,
-                 headers={'User-Agent': 'Mozilla'})
-r.encoding = "utf-8"
-html = r.json()['items_html']
 
-print html
+print gettw('%23%E3%83%90%E3%83%B3%E3%83%89%E3%83%AA%E5%8D%94%E5%8A%9B%20%23%E3%82%AC%E3%83%AB%E3%83%91%E5%8D%94%E5%8A%9B')[1]
+#
+
+# print c
+
+# f = PyQuery(c)('.TweetTextSize').text()
+
+# print f  # ('.TweetTextSize  js-tweet-text tweet-text').html()
+
+
