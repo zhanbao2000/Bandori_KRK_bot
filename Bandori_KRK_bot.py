af74d371668081d7c8c2aa2d42f6bda313640ac5
zhanbao2000 <zhanbao2000@gmail.com>
2019-05-11 23:28:03 +0800

    1.2.0
    正式版本

--- Bandori_KRK_bot.py
+++ Bandori_KRK_bot.py
@@ -4,9 +4,8 @@
 # https://blog.csdn.net/umi_sensei/article/details/78508074
 
 import telebot
-import logging
-import sys
 import time
+import sys
 from config import *
 from tw_spider import gettw
 
@@ -14,21 +13,31 @@ from tw_spider import gettw
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-# logging，控制台输出，用来debug
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 bot = telebot.TeleBot(TOKEN)
 Bandori_keyword = '%23%E3%83%90%E3%83%B3%E3%83%89%E3%83%AA%E5%8D%94%E5%8A%9B%20%23%E3%82%AC%E3%83%AB%E3%83%91%E5%8D%94%E5%8A%9B'
 
 
 def gettime(timestamp):
+    """将Unix时间戳转化为可读的时间.
+
+    :param timestamp: Unix时间戳.
+    :type timestamp: str or int
+    :return: 可读的时间
+    :rtype: int
+    """
     time_local = time.localtime(int(timestamp))
     result = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
     return result
 
 
 def is_number(s):
+    """判断字符串是否为数字.
+
+    :param s: 待验证字符串.
+    :type s: str or number(int, float etc.)
+    :return: 布尔值，字符串是否为数字
+    :rtype: bool
+    """
     try:
         float(s)
         return True
@@ -50,8 +59,8 @@ def tw_handle(word, n):
         for i in range(1, len(char) - 1, 3):
             result += '---- ---- ---- ----\n来自 %s 的推车：\n%s(%d秒前)\n\n%s\n\n\n' % (
                 char[i + 2], gettime(char[i + 1]), now - int(char[i + 1]), char[i])
-            if char[0] != 0:
-                result += "\n已经丢弃%d条无效推文" % char[0]
+        if char[0] != 0:
+            result += "\n已经丢弃%d条无效推文" % char[0]
     return result
 
 
@@ -64,33 +73,34 @@ def verify(chat_id):
 def send_welcome(message):
     bot.send_chat_action(message.chat.id, 'typing')
     bot.send_message(message.chat.id,
-                     '这是一个给邦邦日服玩家使用的bot\n\n你使用 /search 命令后，它会在推特上自动搜寻包含\n#ガルパ協力 #バンドリ協力\n标签的推文并返回给用户\n\n需要帮助请使用 /help 指令',
+                     '这是一个给邦邦日服玩家使用的bot\n\n你使用 /search 或者 /search5 命令后，它会在推特上自动搜寻包含\n#ガルパ協力 #バンドリ協力\n标签的推文并返回给用户\n\n需要帮助请使用 /help 指令',
                      parse_mode='Markdown')
 
 
 @bot.message_handler(commands=["help"])
 def help(message):
-    if verify(message.chat.id + 1):
-        char = """
+    char = """
 可用的指令：
 /start - 开始
 /help - 帮助
 /search - 查找推车
+/search5 - 快速搜寻最新的5辆推车
 """
-        bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, char, )
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(message.chat.id, char, )
 
 
+# 一般方法查找推车
 @bot.message_handler(commands=["search"])
 def search(message):
     number = message.text[8:]
     if not is_number(number):
         bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, '请输入1至20之间的数字！')
+        bot.send_message(message.chat.id, '请附加1至20之间的数字，如 /search 8')
         return
     elif int(number) < 1 or int(number) > 20:
         bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, '请输入1至20之间的数字！')
+        bot.send_message(message.chat.id, '请附加1至20之间的数字，如 /search 8')
         return
 
     bot.send_chat_action(message.chat.id, 'typing')
@@ -109,8 +119,3 @@ def search(message):
 if __name__ == '__main__':
     bot.polling()
 
-'''
-来自 xxxxxxx(@XXXXXXX) 的推车
-发布时间：XXXXXXXXXX（在X秒前）
-内容：XXXXXXXX
-'''
